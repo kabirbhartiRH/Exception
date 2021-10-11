c63a93844fa4ca51149e980bd3af867a4d4b0bff
Kabir Bharti <kbharti@redhat.com>
2021-10-11 14:03:59 -0500

    Java Exception app

---
+++ Exception.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<String> list = new ArrayList<String>();
+		list.get(1);
+		
+		
+	}
+
+}
+
